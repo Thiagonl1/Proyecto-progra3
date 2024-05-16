9052d1952bea93f0c2e0a5c94462906e6c842a01
Thiagonl1 <thiagoll284@gmail.com>
2024-05-16 17:06:55 -0300

    saracatunga

---
+++ src/persona.java
@@ -0,0 +1,14 @@
+public class persona {
+    int edad,
+    dni;
+    String nombre;
+
+    persona(int edad, int dni, String nombre){
+        this.edad=edad;
+        this.dni=dni;
+        this.nombre=nombre;
+    }
+
+
+
+}
