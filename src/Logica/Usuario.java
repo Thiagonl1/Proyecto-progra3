47f97d719d78a3c094c1c26bf23347e54100b3ed
Thiagonl1 <thiagoll284@gmail.com>
2024-06-11 12:49:13 -0300

    new file:   .idea/dbnavigator.xml 	modified:   src/Logica/Usuario.java

--- src/Logica/Usuario.java
+++ src/Logica/Usuario.java
@@ -50,32 +50,32 @@ public class Usuario implements Juego{
     public void verificar(Usuario ia, int desicionPlayer, int desicionIA){
         if(ia.cartas.get(desicionIA).getTipo().equals(this.cartas.get(desicionPlayer).getTipo())){
             System.out.println("Se empato");
-        }else if(this.cartas.get(desicionPlayer).getTipo().equals("Piedra") || ia.cartas.get(desicionIA).getTipo().equals("Piedra")){
+        }else if(this.cartas.get(desicionPlayer).getTipo().equals("Piedra")){
             if(ia.cartas.get(desicionIA).getTipo().equals("Papel")){
                 this.setEstrellas(this.getEstrellas()-1);
                 ia.setEstrellas(ia.getEstrellas()+1);
                 System.out.println("Perdiste");
-            }else if(this.cartas.get(desicionPlayer).getTipo().equals("Papel")){
+            }else{
                 System.out.println("Ganaste");
                 this.setEstrellas(this.getEstrellas()+1);
                 ia.setEstrellas(ia.getEstrellas()-1);
             }
-        }else if(ia.cartas.get(desicionIA).getTipo().equals("Papel") || this.cartas.get(desicionPlayer).getTipo().equals("Papel")){
+        }else if(this.cartas.get(desicionPlayer).getTipo().equals("Papel")){
             if(ia.cartas.get(desicionIA).getTipo().equals("Tijera")){
                 this.setEstrellas(this.getEstrellas()-1);
                 ia.setEstrellas(ia.getEstrellas()+1);
                 System.out.println("Perdiste");
-            }else if(this.cartas.get(desicionPlayer).getTipo().equals("Tijera")){
+            }else{
                 System.out.println("Ganaste");
                 this.setEstrellas(this.getEstrellas()+1);
                 ia.setEstrellas(ia.getEstrellas()-1);
             }
-        }else if(this.cartas.get(desicionPlayer).getTipo().equals("Tijera") || ia.cartas.get(desicionIA).getTipo().equals("Tijera")){
+        }else if(this.cartas.get(desicionPlayer).getTipo().equals("Tijera")){
             if(ia.cartas.get(desicionIA).getTipo().equals("Piedra")){
                 this.setEstrellas(this.getEstrellas()-1);
                 ia.setEstrellas(ia.getEstrellas()+1);
                 System.out.println("Perdiste");
-            }else if(this.cartas.get(desicionPlayer).getTipo().equals("Piedra")){
+            }else{
                 System.out.println("Ganaste");
                 this.setEstrellas(this.getEstrellas()+1);
                 ia.setEstrellas(ia.getEstrellas()-1);
@@ -90,7 +90,7 @@ public class Usuario implements Juego{
         for(int i = 0 ; i < this.cartas.size() ; i++){
             System.out.println(i+")"+this.cartas.get(i).getTipo());
         }
-        // agrego desicion
+        // agrego desicion  
         int desicion = scan.nextInt();
         //Implementar interfaz visual y no consola
         do{
